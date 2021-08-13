30d7a88034e51690d3b8d3f039f09fc0b9e5ecd5
huangkai <h1770360848@outlook.com>
2021-08-13 08:15:53 +0800

    feat(infrastructure): sqlalchemy unit of work implementation

--- seedwork/infrastructure/persistence/sqlalchemy/unit_of_work.py
+++ seedwork/infrastructure/persistence/sqlalchemy/unit_of_work.py
@@ -4,24 +4,24 @@ from typing import Optional
 from sqlalchemy.orm import Session, sessionmaker
 from typing_extensions import Self
 
-from seedwork.domain.unit_of_work import AbstractUnitOfWork
-
 __all__ = ['SQLAlchemyUnitOfWork']
 
 
-class SQLAlchemyUnitOfWork(AbstractUnitOfWork):
+class SQLAlchemyUnitOfWork:
+    """SQLAlchemy unit of work implementation."""
+
     def __init__(self, session_factory: sessionmaker) -> None:
         self.session_factory: sessionmaker = session_factory
 
-    def _commit(self) -> None:
-        self.session.commit()
+    def commit(self) -> None:
+        self._commit()
 
-    def _rollback(self) -> None:
-        self.session.rollback()
+    def rollback(self) -> None:
+        self._rollback()
 
     def __enter__(self) -> Self:
         self.session: Session = self.session_factory()
-        return super().__enter__()
+        return self
 
     def __exit__(
         self,
@@ -29,5 +29,11 @@ class SQLAlchemyUnitOfWork(AbstractUnitOfWork):
         exc_val: Optional[BaseException],
         exc_tb: TracebackException,
     ) -> None:
-        super().__exit__(exc_type, exc_val, exc_tb)
+        self.session.rollback()
         self.session.close()
+
+    def _commit(self) -> None:
+        self.session.commit()
+
+    def _rollback(self) -> None:
+        self.session.rollback()
