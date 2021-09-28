ec9c4ec83ae85917251a05522abfaf88eed323ae
huangkai <h1770360848@outlook.com>
2021-09-28 13:51:52 +0800

    refactor(domain): domain event occurs in aggregate root

--- seedwork/domain/entity.py
+++ seedwork/domain/entity.py
@@ -4,8 +4,6 @@ from typing import Any, Union
 
 from typing_extensions import TypeAlias
 
-from seedwork.domain.event import Event
-
 EntityId: TypeAlias = Union[int, str]
 
 
@@ -18,7 +16,6 @@ class Entity:
 
     Attributes:
         id: Unique identifier of the entity.
-        events: Domain events that occurred on the entity.
 
     See Also:
         - https://enterprisecraftsmanship.com/posts/entity-base-class/
@@ -26,7 +23,6 @@ class Entity:
     """
 
     id: EntityId = field(init=False)
-    events: list[Event] = field(init=False, default_factory=list)
 
     def __post_init__(self) -> None:
         self.id = self.next_id()
@@ -34,12 +30,6 @@ class Entity:
     @classmethod
     def next_id(cls) -> EntityId:
         return str(uuid.uuid4())
-
-    def add_event(self, event: Event) -> None:
-        self.events.append(event)
-
-    def remove_event(self, event: Event) -> None:
-        self.events.remove(event)
 
     def __eq__(self, other: Any) -> bool:
         return other.__class__ is self.__class__ and self.id == other.id
